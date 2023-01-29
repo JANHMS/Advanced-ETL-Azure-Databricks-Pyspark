7ae787564076ed85eda3221a2e96447014c29b44
JANHMS <JANHMS>
2023-01-29 16:23:33 +0000

    feat: ETL with Delta Lake within Databricks
    This PR does a similar logic, as the other, where we did incremental load, as well as the full load in the beginning. However it is doing this incremental load with the usage of Delta Tables on top of it within Databricks.

---
+++ Delta-Lake/ingestion/0.ingest_all_files.py
@@ -0,0 +1,66 @@
+# Databricks notebook source
+v_result = dbutils.notebook.run("1.ingest_circuits_file", 0, {"p_data_source": "Jan Heimes", "p_file_date": "2021-03-28"})
+
+# COMMAND ----------
+
+v_result
+
+# COMMAND ----------
+
+v_result = dbutils.notebook.run("2.ingest_races_file", 0, {"p_data_source": "Jan Heimes", "p_file_date": "2021-03-28"})
+
+# COMMAND ----------
+
+v_result
+
+# COMMAND ----------
+
+v_result = dbutils.notebook.run("3.ingest_constructors_file", 0, {"p_data_source": "Jan Heimes", "p_file_date": "2021-03-28"})
+
+# COMMAND ----------
+
+v_result
+
+# COMMAND ----------
+
+v_result = dbutils.notebook.run("4.ingest_drivers_file", 0, {"p_data_source": "Jan Heimes", "p_file_date": "2021-03-28"})
+
+# COMMAND ----------
+
+v_result
+
+# COMMAND ----------
+
+v_result = dbutils.notebook.run("5.ingest_results_file", 0, {"p_data_source": "Jan Heimes", "p_file_date": "2021-03-28"})
+
+# COMMAND ----------
+
+v_result
+
+# COMMAND ----------
+
+v_result = dbutils.notebook.run("6.ingest_pit_stops_file", 0, {"p_data_source": "Jan Heimes", "p_file_date": "2021-03-28"})
+
+# COMMAND ----------
+
+v_result
+
+# COMMAND ----------
+
+v_result = dbutils.notebook.run("7.ingest_lap_times_file", 0, {"p_data_source": "Jan Heimes", "p_file_date": "2021-03-28"})
+
+# COMMAND ----------
+
+v_result
+
+# COMMAND ----------
+
+v_result = dbutils.notebook.run("8.ingest_qualifying_file", 0, {"p_data_source": "Jan Heimes", "p_file_date": "2021-03-28"})
+
+# COMMAND ----------
+
+v_result
+
+# COMMAND ----------
+
+
